4e853ec87261fd0c0c3ffbe24515a98e75e0dea9
joshtyf <tan.yin.feng@hotmail.com>
2020-08-02 22:58:37 +0800

    added meal credits and uhms links

--- main.py
+++ main.py
@@ -22,14 +22,26 @@ def start(update, context):
     text = "Hello fellow Garudian! I am still a work in progress, so please treat me nicely"
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
+def get_uhms_link(update, context):
+    text = "Here is the uhms link: https://uhms.nus.edu.sg"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+def get_meal_credits_link(update, context):
+    text = "Here is the link to check your meal credits: https://aces.nus.edu.sg/Prjhml/login.do" 
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 start_handler = CommandHandler('start', start)
+uhms_link_handler = CommandHandler('get_uhms_link, get_uhms_link', get_uhms_link)
+meal_credits_handler = CommandHandler('get_meal_credits_link', get_meal_credits_link)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
 # Note to self: order of adding the handlers are important
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(uhms_link_handler)
+dispatcher.add_handler(meal_credits_handler)
 dispatcher.add_handler(unknown_handler)
 
 PORT = int(os.environ.get('PORT', '8443'))
